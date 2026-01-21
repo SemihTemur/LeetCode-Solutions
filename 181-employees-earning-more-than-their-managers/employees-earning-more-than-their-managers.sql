7bc74ed1a43fefc846c4b382e7c679b6a8b903c6
Semih Temur <130358025+SemihTemur@users.noreply.github.com>
2026-01-21 19:38:48 +0300

    Time: 263 ms (25.83%) | Memory: 0B (100.00%) - LeetSync

--- 181-employees-earning-more-than-their-managers/employees-earning-more-than-their-managers.sql
+++ 181-employees-earning-more-than-their-managers/employees-earning-more-than-their-managers.sql
@@ -1,3 +1,4 @@
 -- Write your PostgreSQL query statement below
-SELECT e1.name AS Employee FROM Employee e1 ,Employee e2
- WHERE e2.id = e1.managerId AND e2.salary < e1.salary 
+SELECT e1.name AS Employee FROM Employee e1 INNER JOIN Employee e2
+ON e2.id = e1.managerId 
+WHERE  e2.salary < e1.salary 
