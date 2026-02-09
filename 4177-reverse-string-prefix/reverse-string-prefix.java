2cd77c5a264b44434ccdd51203bacb34e6f09010
Semih Temur <130358025+SemihTemur@users.noreply.github.com>
2026-02-09 23:50:17 +0300

    Time: 1 ms (99.80%) | Memory: 44.5 MB (86.85%) - LeetSync

--- 4177-reverse-string-prefix/reverse-string-prefix.java
+++ 4177-reverse-string-prefix/reverse-string-prefix.java
@@ -1,17 +1,14 @@
 class Solution {
     public String reversePrefix(String s, int k) {
-        int length = s.length() + 1 ;
-        int rotate = k % length;
-
-        int i = 0;
+        int start = 0,end = k -1;
         char[] newString = s.toCharArray();
 
-        while(i<rotate-1){
-            char temp = newString[i];
-            newString[i] = newString[rotate-1];
-            newString[rotate-1] = temp;
-            i++;
-            rotate--;
+        while(start<end){
+            char temp = newString[start];
+            newString[start] = newString[end];
+            newString[end] = temp;
+            start++;
+            end--;
         }
 
         return new String(newString);
