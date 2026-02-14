f383a18213039be47482e7d0fb20e51329f7d6a6
Semih Temur <130358025+SemihTemur@users.noreply.github.com>
2026-02-14 13:40:43 +0300

    Time: 224 ms (91.32%) | Memory: 0B (100.00%) - LeetSync

--- 1837-daily-leads-and-partners/daily-leads-and-partners.sql
+++ 1837-daily-leads-and-partners/daily-leads-and-partners.sql
@@ -1,6 +1,8 @@
 -- Write your PostgreSQL query statement below
-SELECT d.date_id,d.make_name,
-COUNT(DISTINCT d.lead_id) AS unique_leads,
-COUNT(DISTINCT d.partner_id) AS unique_partners 
+SELECT 
+d.date_id,
+d.make_name,
+COUNT(DISTINCT d.lead_id) as unique_leads,
+COUNT(DISTINCT d.partner_id) as unique_partners 
 FROM DailySales d
 GROUP BY d.date_id,d.make_name
